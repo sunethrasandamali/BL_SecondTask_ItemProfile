9bd6b9cda835cac990d179dc431d6ed532b2b09a
hirashdev <hirashdev@bluelotus360.co>
2022-09-23 23:58:48 +0530

    W

---
+++ BlueLotus360.Web.API/Validatoors/IAPIBindingValidator.cs
@@ -0,0 +1,12 @@
+﻿namespace BlueLotus360.Web.API.Validatoors
+{
+    public interface IAPIBindingValidator
+    {
+         
+    }
+
+    public class APIValidationResponse
+    {
+       
+    }
+}
