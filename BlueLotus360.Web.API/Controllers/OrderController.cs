d750df17378ddd523ab624aadc31101a11f7e786
buddhi <buddhi@bluelotus360.com>
2022-09-29 11:00:19 +0530

    1st commit

---
+++ BlueLotus360.Web.API/Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using BlueLotus360.Web.API.Authentication;
+using BlueLotus360.Web.APIApplication.Definitions.ServiceDefinitions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlueLotus360.Web.API.Controllers
+{
+    [BLAuthorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        ILogger<OrderController> _logger;
+        IOrderService _orderService;
+        public OrderController(ILogger<OrderController> logger, IOrderService orderService)
+        {
+            _logger = logger;
+            _orderService = orderService;
+        }
+    }
+}
---
+++ BlueLotus360.Web.APIApplication/Definitions/ServiceDefinitions/IOrderService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueLotus360.Web.APIApplication.Definitions.ServiceDefinitions
+{
+    public interface IOrderService
+    {
+    }
+}
