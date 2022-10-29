bcccc47c55e810b58099c4d8ef424744f8d66fcf
buddhi <buddhi@bluelotus360.com>
2022-10-30 03:21:28 +0530

    commit

--- BlueLotus360.Core.Domain/Definitions/Repository/ICodeBaseRepository.cs
+++ BlueLotus360.Core.Domain/Definitions/Repository/ICodeBaseRepository.cs
@@ -15,5 +15,6 @@ namespace BlueLotus360.Core.Domain.Definitions.Repository
         BaseServerResponse<CodeBaseResponse> GetCodeByOurCodeAndConditionCode(Company company, User user, string OurCode, string ConditionCode);
         BaseServerResponse<IList<CodeBaseResponse>> GetCodeBaseByObject(Company company, User user, ComboRequestDTO requestDTO);
         BaseServerResponse<CodeBaseResponse> GetControlConditionCode(Company company, User user, int ObjectKey, string TableName);
+        BaseServerResponse<IList<CodeBase>> ReadCategories(Company company, User user, ComboRequestDTO requestDTO);
     }
 }
