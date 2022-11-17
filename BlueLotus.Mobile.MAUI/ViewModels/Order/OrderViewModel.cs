3a1b48e75c1b89344b9c811ee7f7b661ca51c650
hirashdev <hirashdev@bluelotus360.co>
2022-11-17 11:53:11 +0530

    c

---
+++ BlueLotus.Mobile.MAUI/ViewModels/Order/OrderViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueLotus.Mobile.MAUI.ViewModels.Order
+{
+    public partial class OrderViewModel:BaseViewModel
+    {
+        [ObservableProperty]
+        private DateTime transactionDate;
+
+        [ObservableProperty]
+        private string trancsactionNumber;
+
+
+    }
+}
