1d7c11b9ad4118c666b2c82cb3cc7cd294edcfdf
hirashdev <hirashdev@bluelotus360.co>
2022-11-17 09:04:25 +0530

    Ca

---
+++ com.bluelotus360.mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace com.bluelotus360.mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
